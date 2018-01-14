aa072d1f2d203421eeb8201eda8f961e166e146a
h404bi <h404bi@gmail.com>
2018-01-14 15:18:59 +0800

    Open source site configs (nginx conf, update webhook, etc)

---
+++ config/h404bi.com.hook.lua
@@ -0,0 +1,23 @@
+local signature = ngx.req.get_headers()["X-Hub-Signature"]
+local key = "" -- GitHub webhook token
+if signature == nil then
+  return ngx.exit(404)
+end
+
+-- HTTP header check
+ngx.req.read_body()
+local t = {}
+for k, v in string.gmatch(signature, "(%w+)=(%w+)") do
+  t[k] = v
+end
+
+local str = require "resty.string"
+local digest = ngx.hmac_sha1(key, ngx.req.get_body_data())
+if not str.to_hex(digest) == t["sha1"] then
+  return ngx.exit(404)
+end
+
+-- Perform update
+local rv = io.popen("/bin/bash /data/scripts/h404bi.com.pull.sh")
+ngx.say(rv:read("*all"))
+ngx.exit(200)
---
+++ config/h404bi.com.pull.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+wwwdir=/data/wwwroot/h404bi.com
+git=/usr/bin/git
+branch=gh-pages
+
+cd $wwwdir
+$git reset --hard origin/$branch
+$git clean -f
+$git pull
+
+dtime=`date`
+echo "site update success at $dtime."
