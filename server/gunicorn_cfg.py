880f7777ee502c36964d8c82e74b6819543a9380
h404bi <h404bi@gmail.com>
2018-10-14 18:22:48 +0800

    remove server

--- server/gunicorn_cfg.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = 'unix:/tmp/h404bi.com.server.sock'
-workers = multiprocessing.cpu_count() * 1
-worker_class = 'gevent'
-accesslog = '/tmp/h404bi.com.server-access.log'
-errorlog = '/tmp/h404bi.com.server-error.log'
-loglevel = 'info'
--- server/manager.py
+++
@@ -1,9 +0,0 @@
-from app import api
-from flask import Flask
-
-entry = Flask(__name__)
-entry.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-entry.register_blueprint(api)
-
-if __name__ == '__main__':
-    entry.run(debug=True)
