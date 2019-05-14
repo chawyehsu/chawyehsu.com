07177a96fef099658cae0433d3df5f732b29962a
h404bi <h404bi@outlook.com>
2019-05-14 23:01:21 +0800

    fix(travis): fix deploy script

--- .travis/deploy-ci.sh
+++ .travis/deploy-ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Decrypt the deploy key
-openssl aes-256-cbc -K $encrypted_e40f4962dafa_key -iv $encrypted_e40f4962dafa_iv -in $WORKINGDIR/.travis/secrets.tar.enc -out $WORKINGDIR/.travis/secrets.tar -d
-tar xvf $WORKINGDIR/.travis/secrets.tar -C $HOME/.ssh/ >/dev/null
+openssl aes-256-cbc -K $encrypted_e40f4962dafa_key -iv $encrypted_e40f4962dafa_iv -in .travis/secrets.tar.enc -out .travis/secrets.tar -d
+tar xvf .travis/secrets.tar -C $HOME/.ssh/ >/dev/null
 # Set the permission of the key
 chmod 600 $HOME/.ssh/deploy_key.pri
 # Start SSH agent
@@ -13,13 +13,13 @@ ssh-add $HOME/.ssh/deploy_key.pri
 git config --global user.name "h404bi"
 git config --global user.email "h404bi@gmail.com"
 # Clone deploy history (keep build commits)
-git clone --depth=1 --branch=gh-pages git@github.com:h404bi/www.h404bi.com.git $WORKINGDIR/.deploy_git
+git clone --depth=1 --branch=gh-pages git@github.com:h404bi/www.h404bi.com.git .deploy_git
 # Clear old files
-rm -rf $WORKINGDIR/.deploy_git/*
+rm -rf .deploy_git/*
 # Copy new files
-cp -r $WORKINGDIR/public/* $WORKINGDIR/.deploy_git
+cp -r public/* .deploy_git
 # Publish new commit to gh-pages
-cd $WORKINGDIR/.deploy_git
+cd .deploy_git
 git add -A
 COMMITDATE=`date '+%Y-%m-%d %H:%M:%S'`
 git commit -m "Site updated at $COMMITDATE"
