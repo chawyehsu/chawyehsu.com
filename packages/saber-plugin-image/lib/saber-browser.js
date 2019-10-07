7415849dc945dc72b654780fc7a366c3b3beaac1
Chawye Hsu <h404bi@outlook.com>
2019-10-07 10:16:03 +0800

    refactor(plugin-image): Sync part of changes from upstream

--- packages/saber-plugin-image/lib/saber-browser.js
+++ packages/saber-plugin-image/lib/saber-browser.js
@@ -2,110 +2,92 @@ import lozad from 'lozad'
 import styles from './styles.module.css'
 
 export default ({ Vue }) => {
-  const options = Object.assign(
-    __SABER_IMAGE_OPTIONS__ || {} // eslint-disable-line no-undef
-  )
+  const options = __SABER_IMAGE_OPTIONS__ // eslint-disable-line no-undef
+  const blank =
+    'data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs='
 
-  if (process.browser) {
-    if (options.polyfill) {
-      // IntersectionObserver polyfill for lozad.js
-      require('intersection-observer')
-    }
-
-    // Mount Lozad instance to Vue
-    // eslint-disable-next-line no-param-reassign
-    Vue.lozad = Vue.prototype.$lozad = lozad('.lozad', {
-      loaded: function(el) {
-        el.onload = function() {
-          el.classList.add(styles.loaded)
-        }
-      }
-    })
-  }
-
-  // Render saber-image
   Vue.component('saber-image', {
     props: ['src', 'lazy'],
-    mounted () {
-      // Observe elements with default selector as '.lozad'
-      this.$nextTick(() =>{
-        this.$lozad.observe()
-      })
+    mounted() {
+      const { $el } = this
+
+      if ($el.dataset.src || $el.dataset.srcset) {
+        lozad($el, {
+          loaded: el => {
+            el.addEventListener(
+              'load',
+              () => {
+                el.setAttribute('data-lazy-loaded', '')
+                el.addEventListener(
+                  'transitionend',
+                  () => {
+                    el.classList.remove(styles.blendIn)
+                  },
+                  { once: true }
+                )
+              },
+              { once: true }
+            )
+          }
+        }).observe()
+      }
     },
-    render (h) {
-      // lazyOptions
-      const lazyOptions = Object.assign(
+    render(h) {
+      const lazy = Object.assign(
         options,
         JSON.parse(this.$attrs['data-lazy'] || '{}'),
         this.lazy
       )
-      // Helper
+
       const getOption = key =>
-        lazyOptions[key] || (lazyOptions[key] !== false && options[key])
+        lazy[key] || (lazy[key] !== false && options[key])
 
       const { $attrs } = this
 
-      // LazyLoad enabled
       if (getOption('lazyLoad')) {
-        // BlendIn effect
-        const blendIn = getOption('blendIn')
-
-        // Default lazyload placeholder
-        let loader = 'data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs='
-
-        if (getOption('placeholder') && typeof lazyOptions.placeholder === 'string') {
-          loader = lazyOptions.placeholder
-        }
-
-        // Responsive images disabled
         if (typeof this.src === 'string') {
-          return h('img', {
-            attrs: {
-              ...$attrs,
-              src: this.src,
-              srcset: loader,
-              'data-srcset': this.src,
-              'data-pswp-title': $attrs.alt
-            },
-            class: { lozad, [styles.blendIn]: blendIn }
-          })
-        } else {
-          // Responsive images enabled
-          // See: https://github.com/herrstucki/responsive-loader#usage
-          const { width, src, srcSet, placeholder } = this.src
-
-          if (getOption('placeholder') && placeholder) {
-            loader = placeholder
-          }
+          const { src } = this
 
           return h('img', {
             attrs: {
               ...$attrs,
               src,
-              srcset: loader,
-              'data-srcset': srcSet,
-              width,
-              height: 'auto',
+              srcset: blank,
+              'data-srcset': src,
               'data-pswp-title': $attrs.alt
-            },
-            class: { lozad, [styles.blendIn]: blendIn }
-          })
-        }
-      // lazyLoad disabled
-      } else {
-        // Responsive images disabled
-        if (typeof this.src === 'string') {
-          return h('img', {
-            attrs: { ...$attrs, src: this.src, 'data-pswp-title': $attrs.alt }
+            }
           })
         }
 
-        // Responsive images enabled
-        const { src, srcSet: srcset } = this.src
+        const { width, src, srcSet, placeholder } = this.src || {}
+
+        const loading = getOption('placeholder') ? placeholder : blank
+
+        const blendIn = getOption('blendIn')
+
         return h('img', {
-          attrs: { ...$attrs, src, srcset, 'data-pswp-title': $attrs.alt }
+          attrs: {
+            ...$attrs,
+            src,
+            srcset: loading,
+            'data-srcset': srcSet,
+            'data-pswp-title': $attrs.alt,
+            width
+          },
+          class: { [styles.blendIn]: blendIn }
         })
       }
+
+      if (typeof this.src === 'string') {
+        return h('img', {
+          attrs: { ...$attrs, src: this.src, 'data-pswp-title': $attrs.alt }
+        })
+      }
+
+      const { src, srcSet: srcset } = this.src || {}
+      return h('img', {
+        attrs: { ...$attrs, src, srcset, 'data-pswp-title': $attrs.alt }
+      })
     }
   })
 }
