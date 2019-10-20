be90bf28043cae5f10c66ac41574c161d3b628af
Chawye Hsu <h404bi@outlook.com>
2019-10-20 17:53:16 +0800

    release(plugin): Publish
    saber-plugin-google-adsense@0.2.0

--- packages/saber-plugin-google-adsense/index.js
+++ packages/saber-plugin-google-adsense/index.js
@@ -4,11 +4,40 @@ const ID = 'google-adsense'
 
 exports.name = ID
 
-exports.apply = (api, { adClientId = false } = {}) => {
+exports.apply = (api, options = {}) => {
+  // Ad-client-id for testing
+  const TEST_AD_CLIENT_ID = 'ca-google'
+
+  options = Object.assign(
+    {
+      // Adsense script URL
+      adsenseScriptUrl: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+      adClientId: null,
+      pageLevelAds: false,
+      test: false
+    },
+    options
+  )
+
+  if (process.env.NODE_ENV !== 'production') {
+    // If in DEV mode, place ads in 'test' state automatically
+    options.test = true
+  }
+
+  if (options.test) {
+    // If in test mode, we use the Test Client ID
+    options.adClientId = TEST_AD_CLIENT_ID
+  }
+
+  if (!options.adClientId || typeof options.adClientId !== 'string') {
+    // Invalid adsense client ID, so don't include
+    return
+  }
+
   api.hooks.chainWebpack.tap(ID, config => {
-    config.plugin('constants').tap(([options]) => [
-      Object.assign(options, {
-        __AD_CLIENT_ID__: JSON.stringify(adClientId)
+    config.plugin('constants').tap(([constants]) => [
+      Object.assign(constants, {
+        __SABER_GOOGLE_ADSENSE_OPTIONS__: JSON.stringify(options)
       })
     ])
   })
--- packages/saber-plugin-google-adsense/saber-browser.js
+++ packages/saber-plugin-google-adsense/saber-browser.js
@@ -1,12 +1,121 @@
-export default function () {
-  if (process.browser && process.env.NODE_ENV === 'production' && __AD_CLIENT_ID__) {
+export default function ({ Vue }) {
+  const options = __SABER_GOOGLE_ADSENSE_OPTIONS__ // eslint-disable-line no-undef
+
+  // Import Google Adsense script
+  if (process.browser && options['adClientId']) {
     (function () {
       var ads = document.createElement('script')
-      ads.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
       ads.async = true
-      ads.setAttribute('data-ad-client', __AD_CLIENT_ID__)
-      var s = document.getElementsByTagName("script")[0]
+      ads.src = options['adsenseScriptUrl']
+      ads.onload = () => {
+        (window.adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: options['adClientId'],
+          enable_page_level_ads: options['pageLevelAds']
+        })
+      }
+      var s = document.getElementsByTagName('script')[0]
       s.parentNode.insertBefore(ads, s)
     })()
   }
+
+  // Register ad component
+  Vue.component('adsbygoogle', {
+    props: {
+      adClient: {
+        type: String,
+        default: options['adClientId']
+      },
+      adSlot: {
+        type: String
+      },
+      adFormat: {
+        type: String,
+        default: 'auto'
+      },
+      adLayout: {
+        type: String
+      },
+      adLayoutKey: {
+        type: String
+      },
+      adStyle: {
+        type: Object,
+        default () {
+          return {
+            display: 'block'
+          }
+        }
+      }
+    },
+    data () {
+      return {
+        show: true
+      }
+    },
+    mounted () {
+      this.showAd()
+    },
+    watch: {
+      '$route' (to, from) {
+        if (to.fullPath === from.fullPath) {
+          return
+        }
+
+        let changed = false
+
+        if (to.path !== from.path) {
+          changed = true
+        }
+
+        if (changed) {
+          // If the route has changed, update the ad
+          this.updateAd()
+        }
+      }
+    },
+    methods: {
+      adRegion () {
+        return 'page-' + Math.random()
+      },
+      updateAd () {
+        // Reset the INS element
+        this.show = false
+        // Show new ad on nextTick
+        this.$nextTick(this.showAd)
+      },
+      showAd () {
+        this.show = true
+        this.$nextTick(() => {
+          try {
+            // Once ad container (<ins>) DOM has (re-)rendered, requesst a new advert
+            (window.adsbygoogle = window.adsbygoogle || []).push({})
+          } catch (error) {
+            console.error(error)
+          }
+        })
+      }
+    },
+    render (h) {
+      return h(
+        'ins',
+        {
+          'class': ['adsbygoogle'],
+          style: this.adStyle,
+          attrs: {
+            'data-ad-client': this.adClient,
+            'data-ad-slot': this.adSlot || null,
+            'data-ad-format': this.adFormat,
+            'data-ad-region': this.show ? this.adRegion() : null,
+            'data-ad-layout': this.adLayout || null,
+            'data-ad-layout-key': this.adLayoutKey || null,
+            'data-adtest': options['test'] ? 'on' : null
+          },
+          domProps: {
+            innerHTML: this.show ? '' : ' '
+          },
+          key: Math.random()
+        }
+      )
+    }
+  })
 }
