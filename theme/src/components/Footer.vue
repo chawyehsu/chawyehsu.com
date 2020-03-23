1c6a9107a947404d100dc3b8c465751e9ac14679
Chawye Hsu <h404bi@outlook.com>
2020-03-23 23:44:19 +0800

    refactor(theme): minor tweaks

--- theme/src/components/Footer.vue
+++ theme/src/components/Footer.vue
@@ -3,8 +3,9 @@
     <div class="tach-wrapper">
       <div class="container columns">
         <section class="section column is-6">
+          <!-- &#xfe0e; to prevent rendering ❤︎ as an emoji in iOS/Android -->
           <div class="copyright">
-            © {{ siteDate }} {{ $siteConfig.author }}. Made with <span style="color:#b30e2e">♥</span> in Guangzhou
+            © {{ siteDate }} {{ $siteConfig.author }}. Made with <span style="color:#b30e2e">❤︎&#xfe0e;</span> in Guangzhou
           </div>
         </section>
         <section class="section column is-6" v-if="$themeConfig.footer.social_network">
