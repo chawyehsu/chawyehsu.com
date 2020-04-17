chawyehsu/chawyehsu.com

commit 8d7482bb6d41caa03a2deec2569a4871aacc9e7c

chore(plugin): Don't use jsdeliver cdn for images in dev mode

packages/saber-plugin-image/lib/saber-browser.js
@@ -37,7 +37,7 @@ export default ({ Vue }) => {
     },
     methods: {
       updatedImageUrl (src) {
-        if (jsdeliverCdn && jsdeliverCdnPrefix) {
+        if (process.browser && process.env.NODE_ENV === 'production' && jsdeliverCdn && jsdeliverCdnPrefix) {
           return src.replace('/_saber', `${jsdeliverCdnPrefix.replace(/\/$/,'')}/_saber`)
         } else {
           return src

commit c20c04613bf9a996fbd6269a493b517574db2e59

fix(plugin): Fix regression introduced in 8d7482b

packages/saber-plugin-image/lib/saber-browser.js
@@ -37,7 +37,7 @@ export default ({ Vue }) => {
     },
     methods: {
       updatedImageUrl (src) {
-        if (process.browser && process.env.NODE_ENV === 'production' && jsdeliverCdn && jsdeliverCdnPrefix) {
+        if (process.env.NODE_ENV === 'production' && jsdeliverCdn && jsdeliverCdnPrefix) {
           return src.replace('/_saber', `${jsdeliverCdnPrefix.replace(/\/$/,'')}/_saber`)
         } else {
           return src