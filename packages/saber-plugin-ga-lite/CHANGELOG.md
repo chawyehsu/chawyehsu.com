366201c22d15f5d4a8febd478de93812d04b471d
Chawye Hsu <h404bi@outlook.com>
2020-05-23 11:25:45 +0800

    feat(plugin-ga-lite): respect user's doNotTrack setting

---
+++ packages/saber-plugin-ga-lite/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+## 0.3.0 (2020-05-23)
+
+- **feat**: respect user's doNotTrack setting
--- packages/saber-plugin-ga-lite/saber-browser.js
+++ packages/saber-plugin-ga-lite/saber-browser.js
@@ -1,7 +1,23 @@
 export default function (ctx) {
   const options = __SABER_GALITE_OPTIONS__ // eslint-disable-line no-undef
 
-  if (process.browser && process.env.NODE_ENV === 'production' && options['trackId']) {
+  if (
+    process.browser &&
+    process.env.NODE_ENV === 'production' &&
+    options['trackId']
+  ) {
+    const doNotTrackEnabled = parseInt(
+      navigator.msDoNotTrack || // Internet Explorer 9 and 10 vendor prefix
+      window.doNotTrack || // IE 11 uses window.doNotTrack
+        navigator.doNotTrack, // W3C
+      10
+    ) === 1
+
+    if (doNotTrackEnabled) {
+      // Respect browsers' doNotTrack setting
+      return
+    }
+
     (function (e, t, n, i, s, a, c) {
       e[n] = e[n] || function () {
         (e[n].q = e[n].q || []).push(arguments)
