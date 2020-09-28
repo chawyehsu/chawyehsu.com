chawyehsu/chawyehsu.com

commit c13257e696c5442a8703adbe32ab5cc3542e897f

fix(theme): Fix vue-pswipe causing client side hydration to fail

theme/src/layouts/default.vue
@@ -49,10 +49,12 @@
             本文最后更新于 {{ days }} 天前（{{ humanDate }}），其中的信息可能已经有所发展或者不再适合现阶段。
           </section>
           <section class="page-body">
-            <Photoswipe v-if="$themeConfig.pswp" auto ref="photoswipe">
-              <slot name="default" />
-            </Photoswipe>
-            <slot v-else name="default" />
+            <client-only>
+              <Photoswipe v-if="$themeConfig.pswp" auto ref="photoswipe">
+                <slot name="default" />
+              </Photoswipe>
+              <slot v-else name="default" />
+            </client-only>
           </section>
           <section class="page-block-action" v-if="$themeConfig.share || page.tagsInfo">
             <div class="page-share"></div>

commit 36f38e88016e6b40ae78b6723093f272b2490df2

fix(feed): fix feed regression introduced in c13257e6

theme/src/layouts/default.vue
@@ -49,12 +49,10 @@
             本文最后更新于 {{ days }} 天前（{{ humanDate }}），其中的信息可能已经有所发展或者不再适合现阶段。
           </section>
           <section class="page-body">
-            <client-only>
-              <Photoswipe v-if="$themeConfig.pswp" auto ref="photoswipe">
-                <slot name="default" />
-              </Photoswipe>
-              <slot v-else name="default" />
-            </client-only>
+            <Photoswipe v-if="$themeConfig.pswp" auto ref="photoswipe">
+              <slot name="default" />
+            </Photoswipe>
+            <slot v-else name="default" />
           </section>
           <section class="page-block-action" v-if="$themeConfig.share || page.tagsInfo">
             <div class="page-share"></div>