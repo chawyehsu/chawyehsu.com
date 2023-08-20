cba6c542af53cc5b1f53eb7906dfa2b38fd4cf32
Chawye Hsu <su+git@chawyehsu.com>
2023-08-20 17:41:07 +0800

    fix(theme): no a tag without href
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- theme/components/SchemeSwitcher.vue
+++ theme/components/SchemeSwitcher.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="navbar-item scheme-switcher" @click.prevent="toggleScheme">
+  <div class="navbar-item scheme-switcher" @click.prevent="toggleScheme">
     <span>ok</span>
     <svg
       xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -14,7 +14,7 @@
       <circle cx="12" cy="12" r="5"/>
       <path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/>
     </svg>
-  </a>
+  </div>
 </template>
 
 <script>
