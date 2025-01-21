4a47c5fce8798989cf6faf5f588311047b7402c2
Chawye Hsu <su+git@chawyehsu.com>
2025-01-21 12:40:07 +0800

    chore: test style preloading compatibility with cloudflare fonts
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- theme/saber-browser.js
+++ theme/saber-browser.js
@@ -1,78 +1,79 @@
 import './styles/main.css'
 
-export default ({ Vue, setHead }) => {
+export default ({ __Vue, setHead }) => {
   // Manipulating <head>
   setHead(vm => ({
     htmlAttrs: {
-      lang: vm.$siteConfig.lang
+      lang: vm.$siteConfig.lang,
     },
     meta: [
       {
         name: 'apple-mobile-web-app-capable',
-        content: 'yes'
+        content: 'yes',
       },
       {
         name: 'apple-mobile-web-app-status-bar-style',
-        content: 'default'
+        content: 'default',
       },
       {
         name: 'mobile-web-app-capable',
-        content: 'yes'
+        content: 'yes',
       },
       {
         name: 'author',
-        content: vm.$siteConfig.author
+        content: vm.$siteConfig.author,
       },
       {
         property: 'og:type',
-        content: 'website'
+        content: 'website',
       },
       {
         name: 'twitter:card',
-        content: 'summary'
+        content: 'summary',
       },
       {
         name: 'twitter:creator',
-        content: vm.$siteConfig.author
-      }
+        content: vm.$siteConfig.author,
+      },
     ],
     link: [
       {
         rel: 'alternate',
         title: `${vm.$siteConfig.title} - Feed`,
         href: `${vm.$siteConfig.url}${vm.$feed.permalink}`,
-        type: vm.$feed.type === 'atom' ? 'application/atom+xml' :
-          vm.$feed.type === 'rss2' ? 'application/rss+xml' : 'application/json'
+        type: vm.$feed.type === 'atom'
+          ? 'application/atom+xml'
+          : vm.$feed.type === 'rss2' ? 'application/rss+xml' : 'application/json',
       },
       {
         href: 'https://cdn.jsdelivr.net',
         rel: 'preconnect',
-        crossorigin: true
+        crossorigin: true,
       },
       {
         href: 'https://fonts.googleapis.com',
-        rel: 'preconnect'
+        rel: 'preconnect',
       },
       {
         href: 'https://fonts.gstatic.com',
         rel: 'preconnect',
-        crossorigin: true
+        crossorigin: true,
       },
+      // {
+      //   href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+      //   rel: 'preload',
+      //   as: 'style',
+      // },
       {
         href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap',
-        rel: 'preload',
-        as: 'style'
+        rel: 'stylesheet',
       },
-      {
-        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap',
-        rel: 'stylesheet'
-      }
     ],
     script: [
       {
-        innerHTML: '(()=>{const d="dark";const s=localStorage.getItem("user-color-scheme");const p=window.matchMedia("(prefers-color-scheme: dark)").matches;if(!s?p:s===d){const c=document.documentElement.classList;!c.contains(d)&&c.add(d)}})();'
-      }
+        innerHTML: '(()=>{const d="dark";const s=localStorage.getItem("user-color-scheme");const p=window.matchMedia("(prefers-color-scheme: dark)").matches;if(!s?p:s===d){const c=document.documentElement.classList;!c.contains(d)&&c.add(d)}})();',
+      },
     ],
-    __dangerouslyDisableSanitizers: ['script']
+    __dangerouslyDisableSanitizers: ['script'],
   }))
 }
