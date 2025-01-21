07cbbd5ed7abd2a332535dc9a6e35962e4a308b2
Chawye Hsu <su+git@chawyehsu.com>
2025-01-21 18:01:55 +0800

    chore: lint
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- packages/saber-plugin-image-photoswipe/index.js
+++ packages/saber-plugin-image-photoswipe/index.js
@@ -1,13 +1,13 @@
-const { join } = require('path')
+const { join } = require('node:path')
 const renderAttrs = require('./shim')
 
 const ID = 'saber-plugin-image-photoswipe'
 exports.name = ID
 
-const checkMarkdownPlugin = () => {
+function checkMarkdownPlugin() {
   try {
     require('markdown-it-figure-gallery')
-  } catch (e) {
+  } catch {
     throw new Error('markdown-it-figure-gallery plugin is required.')
   }
 }
@@ -19,35 +19,36 @@ exports.apply = (api, options = {}) => {
       auto: true,
       bubble: false,
       lazy: true,
-      rotate: false
+      rotate: false,
     },
-    options
+    options,
   )
 
   checkMarkdownPlugin()
 
   // Wrap Markdown image/gallery with PhotoSwipe Component
-  api.hooks.chainMarkdown.tap(ID, config => {
-    config.plugin(ID).use(md => {
-
+  api.hooks.chainMarkdown.tap(ID, (config) => {
+    config.plugin(ID).use((md) => {
       // FIXME: apply renderAttrs shim (markdown-it future)
       md.renderer.renderAttrs = renderAttrs
 
-      md.core.ruler.push(ID, state => {
+      md.core.ruler.push(ID, (state) => {
         const tokens = state.tokens
 
-        if (state.tokens.length < 2) return
+        if (state.tokens.length < 2) {
+          return
+        }
 
         // do not process first and last token
         for (let i = 1; i < state.tokens.length - 1; ++i) {
           if (
             (
-              tokens[i - 1].type === 'figure_open' &&
-              (tokens[i + 1].type === 'figure_close')
-            ) ||
-            (
-              tokens[i - 1].type === 'container_gallery_open' &&
-              (tokens[i + 1].type === 'container_gallery_close')
+              tokens[i - 1].type === 'figure_open'
+              && (tokens[i + 1].type === 'figure_close')
+            )
+            || (
+              tokens[i - 1].type === 'container_gallery_open'
+              && (tokens[i + 1].type === 'container_gallery_close')
             )
           ) {
             if (tokens[i - 1].attrGet('data-pswp') === 'true') {
@@ -67,13 +68,13 @@ exports.apply = (api, options = {}) => {
     })
   })
 
-  api.browserApi.add(join(__dirname, 'saber-browser.js'))
-
-  api.hooks.chainWebpack.tap(ID, config => {
+  api.hooks.chainWebpack.tap(ID, (config) => {
     config.plugin('constants').tap(([constants]) => [
       Object.assign(constants, {
-        __SABER_PHOTOSWIPE_OPTIONS__: JSON.stringify(options)
-      })
+        __SABER_PHOTOSWIPE_OPTIONS__: JSON.stringify(options),
+      }),
     ])
   })
+
+  api.browserApi.add(join(__dirname, 'saber-browser.js'))
 }
--- packages/saber-plugin-image-photoswipe/saber-browser.js
+++ packages/saber-plugin-image-photoswipe/saber-browser.js
@@ -1,5 +1,6 @@
 export default ({ Vue }) => {
-  if (process.browser) {
+  if (typeof window !== 'undefined') {
+    // eslint-disable-next-line no-undef
     Vue.use(require('vue-pswipe').default, __SABER_PHOTOSWIPE_OPTIONS__)
   }
 }
